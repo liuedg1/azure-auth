e816528891ce536470e44fbd1d6152af58df2c0e
liuedg <liuedg@hotmail.com>
2025-06-01 17:36:06 +0800

    更改简介

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1541796375"
+    serviceWorkerVersion: "2907451963"
   }
 });
